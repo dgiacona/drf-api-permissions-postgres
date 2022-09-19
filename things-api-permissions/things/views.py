59274f287e0dcb87129343a4f3bf6481cdf38dea
Dominic Giacona <dominic@exobyte.io>
2022-09-19 04:35:06 -0400

    completed lab 32

--- things-api-permissions/things/views.py
+++ things-api-permissions/things/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Thing
+from .permissions import IsOwnerOrReadOnly
+from .serializers import ThingSerializer
 
-# Create your views here.
+
+class ThingList(generics.ListCreateAPIView):
+
+    # Anything that inherits from ListAPI View is going to need 2 things.
+    # What is the collection of things, aka the queryset
+    # Serializer_class
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
+
+
+# The ThingDetail needs the same things
+class ThingDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
